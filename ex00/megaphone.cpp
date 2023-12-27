0a7693b5c090b50f568692be1d10ac3d40889537
Luy Nagda <lunagda@z4r11p3.42lyon.fr>
2023-12-27 18:25:27 +0100

    Starting with CPP

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 15:04:25 by lunagda           #+#    #+#             */
+/*   Updated: 2023/12/27 18:17:43 by lunagda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char) toupper(argv[i][j]);
+			if (i < argc - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
